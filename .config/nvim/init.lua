5da7ee5ed225faf1a7af452eb072d644785a0d74
Beafantles <sebastien.egner.github@gmail.com>
2023-05-04 08:35:10 +0200

    nvim configuration & clang files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,171 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.encoding = 'UTF-8'
+
+-- nvimtree options
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+-- barbar options
+vim.g.barbar_auto_setup = false
+
+vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>s', '<cmd>write<cr>')
+vim.keymap.set({ 'n', 'x' }, 'cp', '"+y')
+vim.keymap.set({ 'n', 'x' }, 'cv', '"+p')
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-tree/nvim-tree.lua')
+Plug('folke/tokyonight.nvim', { branch = 'main' })
+Plug('romgrk/barbar.nvim')
+Plug('akinsho/toggleterm.nvim', { tag = '*' })
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
+
+vim.call('plug#end')
+
+vim.keymap.set('n', '<leader>n', '<cmd>:NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>b', '<cmd>:NvimTreeFocus<cr>')
+vim.keymap.set('n', '<leader>&', '<cmd>:BufferGoto 1<cr>')
+vim.keymap.set('n', '<leader>é', '<cmd>:BufferGoto 2<cr>')
+vim.keymap.set('n', '<leader>"', '<cmd>:BufferGoto 3<cr>')
+vim.keymap.set('n', "<leader>'", '<cmd>:BufferGoto 4<cr>')
+vim.keymap.set('n', '<leader>(', '<cmd>:BufferGoto 5<cr>')
+vim.keymap.set('n', '<leader>-', '<cmd>:BufferGoto 6<cr>')
+vim.keymap.set('n', '<leader>è', '<cmd>:BufferGoto 7<cr>')
+vim.keymap.set('n', '<leader>_', '<cmd>:BufferGoto 8<cr>')
+vim.keymap.set('n', '<leader>ç', '<cmd>:BufferGoto 9<cr>')
+vim.keymap.set('n', '<leader>j', '<cmd>:ToggleTerm size=15 direction=horizontal<cr>')
+vim.keymap.set('n', '<leader>w', '<cmd>:BufferClose<cr>')
+
+vim.cmd[[colorscheme tokyonight-storm]]
+
+require("nvim-tree").setup {
+    live_filter = {
+        always_show_folders = false
+    }
+}
+
+require("barbar").setup {
+    sidebar_filetypes = {
+        NvimTree = true
+    }
+}
+
+require("toggleterm").setup()
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  pattern = "NvimTree_*",
+  callback = function()
+    local layout = vim.api.nvim_call_function("winlayout", {})
+    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+  end
+})
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = { 
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) 
+  end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    }, {
+    { name = 'buffer' },
+  })
+})
+
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, 
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lsp_config = require('lspconfig')
+
+lsp_config.clangd.setup {
+  capabilities = capabilities,
+  root_dir = lsp_config.util.root_pattern(
+            '.clangd',
+            '.clang-tidy',
+            '.clang-format',
+            'build/compile_commands.json',
+            'compile_flags.txt',
+            'configure.ac',
+            '.git')
+}
+
+lsp_config.cmake.setup {
+    capabilities = capabilities
+}
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
