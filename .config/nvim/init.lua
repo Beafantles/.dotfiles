1c821dc096bce7b2576ae93759cf52b4a430a8ac
Sébastien <sebastien.egner.github@gmail.com>
2023-05-09 09:33:04 +0200

    Updated neovim configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,8 +45,44 @@ Plug('preservim/tagbar')
 Plug('nvim-lualine/lualine.nvim')
 Plug('lukas-reineke/indent-blankline.nvim')
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+Plug('m4xshen/autoclose.nvim')
+Plug('iamcco/markdown-preview.nvim', { ['do'] = ':call mkdp#util#install()', ['for'] = 'markdown' })
+Plug('lewis6991/gitsigns.nvim')
+Plug('mg979/vim-visual-multi', { ['branch'] = 'master' })
 
 vim.call('plug#end')
+
+-- multi cursor settings
+vim.g.VM_mouse_mappings = 1
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
 vim.keymap.set('n', '<leader>n', '<cmd>:NvimTreeToggle<cr>')
 vim.keymap.set('n', '<leader>b', '<cmd>:NvimTreeFocus<cr>')
@@ -71,6 +107,8 @@ vim.cmd [[match ExtraWhitespace /\s\+$/]]
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#bf80ff gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineContextStart guisp=#bf80ff gui=underline]]
 
+require("autoclose").setup()
+require("gitsigns").setup()
 require("indent_blankline").setup {
     char = '▏',
     show_current_context = true,
@@ -237,4 +275,3 @@ lsp_config.cmake.setup {
 }
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
-
